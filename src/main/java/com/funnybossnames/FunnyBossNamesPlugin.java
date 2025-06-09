af4491d80544b8a4b0716ae06d5021bcc9aee21b
Lukas Pfanner <lukas.pfann@gmail.com>
2025-06-09 13:36:16 +0200

    removed commented out code

--- src/main/java/com/funnybossnames/FunnyBossNamesPlugin.java
+++ src/main/java/com/funnybossnames/FunnyBossNamesPlugin.java
@@ -37,7 +37,7 @@ public class FunnyBossNamesPlugin extends Plugin
 
 		Widget bossName = client.getWidget(303,9);
 		//Widget colLogName = client.getWidget(621,12);
-		Widget colLogNameBig = client.getWidget(621,19);
+		//Widget colLogNameBig = client.getWidget(621,19);
 		//Widget colLogNameSmall = client.getWidget(621,19);
 		Widget scoreBoardName = client.getWidget(817,6);
 
@@ -47,31 +47,6 @@ public class FunnyBossNamesPlugin extends Plugin
 			{
 				bossName.setText(bossName.getText().replace(bossName.getText(),boss.getNewName()));
 			}
-
-			// Commented out due to not working consistently
-			//if(colLogName != null && colLogName.getChild(27) != null
-			//		&& colLogName.getChild(27).getText().contains(boss.getBossName()))
-			//{
-			//	colLogName = colLogName.getChild(27);
-			//	log.info("colLogName was found");
-			//	colLogName.setText(colLogName.getText().replace(colLogName.getText(),boss.getNewName()));
-			//}
-
-			//if(colLogNameBig != null && colLogNameBig.getChild(0) != null
-			//		&& colLogNameBig.getChild(0).getText().contains(boss.getBossName()))
-			//{
-			//	colLogNameBig = colLogNameBig.getChild(0);
-			//	colLogNameBig.setText(colLogNameBig.getText().replace(colLogNameBig.getText(),boss.getNewName()));
-			//}
-
-			// Commented out due to not working consistently
-			//if(colLogNameSmall != null && colLogNameSmall.getChild(2) != null
-			//		&& colLogNameSmall.getChild(2).getText().contains(boss.getBossName()))
-			//{
-			//	colLogNameSmall = colLogNameSmall.getChild(2);
-			//	log.info("colLogNameSmall was found");
-			//	colLogNameSmall.setText(colLogNameSmall.getText().replace(colLogNameSmall.getText(),boss.getNewName()));
-			//}
 
 			if(scoreBoardName != null && scoreBoardName.getText().contains(boss.getBossName()))
 			{
@@ -99,7 +74,7 @@ public class FunnyBossNamesPlugin extends Plugin
 	}
 
 	@Override
-	protected void startUp() throws Exception
+	protected void startUp()
 	{
 		log.info("Funny boss names plugin started!");
 		bosses = Arrays.asList(Boss.values());
